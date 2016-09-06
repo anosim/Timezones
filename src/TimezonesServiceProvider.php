47467c1915d2de7ee235ba5c14aafe52a703b4ba
anosim <anosim@hotmail.com>
2016-09-06 23:00:31 +0500

    Add files via upload

---
+++ src/TimezonesServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Codesgo\Timezones;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimezonesServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+
+		include __DIR__.'/routes.php';
+		$this->app->make('Codesgo\Timezones\TimezonesController');
+
+	}
+}
---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('timezones/{timezone}', 
+  'codesgo\timezones\TimezonesController@index');
